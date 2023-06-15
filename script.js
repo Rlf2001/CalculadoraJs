962bf2f47198877f5935ba4ce6198fa1f26c75da
Marilyncabirta <113348077+Marilyncabirta@users.noreply.github.com>
2023-06-15 09:24:19 +0200

    Javascript de calculadora

---
+++ script.js
@@ -0,0 +1,27 @@
+var numero1;
+var numero2;
+var operacion;
+
+function iniciar(){
+    //variables
+    var valorAnterior=document.getElementsByClassName('valor-anterior')
+    var valorActual=document.getElementsByClassName('valor-actual')
+    var resultado = document.getElementsByClassName('resultado');
+    var reset = document.getElementBycgetElementsByClassName('borrar');
+    var suma = document.getElementBycgetElementsByClassName('operadorSuma');
+    var resta = document.getElementBycgetElementsByClassName('operadorResta');
+    var multiplicacion = document.getElementBycgetElementsByClassName('operadorMultiplicacion');
+    var division = document.getElementBycgetElementsByClassName('operadorDivision');
+    var igual = document.getElementBycgetElementsByClassName('operadorIgual');
+    var uno = document.getElementBycgetElementsByClassName('numero1');
+    var dos = document.getElementBycgetElementsByClassName('numero2');
+    var tres = document.getElementBycgetElementsByClassName('numero3');
+    var cuatro = document.getElementBycgetElementsByClassName('numero4');
+    var cinco = document.getElementBycgetElementsByClassName('numero5');
+    var seis = document.getElementBycgetElementsByClassName('numero6');
+    var siete = document.getElementBycgetElementsByClassName('numero7');
+    var ocho = document.getElementBycgetElementsByClassName('numero8');
+    var nueve = document.getElementBycgetElementsByClassName('numero9');
+    var cero = document.getElementBycgetElementsByClassName('numero0');
+    var coma=document.getElementsByClassName("numerodecimal");
+  }

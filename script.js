bfba492739fafe2754357c4862cb699d394d6954
Marilyncabirta <113348077+Marilyncabirta@users.noreply.github.com>
2023-06-23 10:33:58 +0200

    script actualizado

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-var numero1;
-var numero2;
-var operacion;
+function concatenarNum(numero) {
+  document.getElementById('valoractual').value+=numero;  
+}
 
-function iniciar(){
-    //variables
-    var valorAnterior=document.getElementsByClassName('valor-anterior')
-    var valorActual=document.getElementsByClassName('valor-actual')
-    var resultado = document.getElementsByClassName('resultado');
-    var reset = document.getElementBycgetElementsByClassName('borrar');
-    var suma = document.getElementBycgetElementsByClassName('operadorSuma');
-    var resta = document.getElementBycgetElementsByClassName('operadorResta');
-    var multiplicacion = document.getElementBycgetElementsByClassName('operadorMultiplicacion');
-    var division = document.getElementBycgetElementsByClassName('operadorDivision');
-    var igual = document.getElementBycgetElementsByClassName('operadorIgual');
-    var uno = document.getElementBycgetElementsByClassName('numero1');
-    var dos = document.getElementBycgetElementsByClassName('numero2');
-    var tres = document.getElementBycgetElementsByClassName('numero3');
-    var cuatro = document.getElementBycgetElementsByClassName('numero4');
-    var cinco = document.getElementBycgetElementsByClassName('numero5');
-    var seis = document.getElementBycgetElementsByClassName('numero6');
-    var siete = document.getElementBycgetElementsByClassName('numero7');
-    var ocho = document.getElementBycgetElementsByClassName('numero8');
-    var nueve = document.getElementBycgetElementsByClassName('numero9');
-    var cero = document.getElementBycgetElementsByClassName('numero0');
-    var coma=document.getElementsByClassName("numerodecimal");
-  }
+function calcularOperacion(){
+  let resultado=eval(document.getElementById('valoractual').value);
+  document.getElementById('valoractual').value=resultado
+
+}
+function borrarNum(){
+  document.getElementById('valoractual').value="";
+}
